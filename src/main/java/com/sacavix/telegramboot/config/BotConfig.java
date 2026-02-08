66e663d8a3434c7124afbe9012e9b2c5086380a3
faridrm14 <ruizmartinezfarid.com>
2026-02-07 23:58:25 -0600

    Initial commit - Telegram Bot Spring Boot

---
+++ src/main/java/com/sacavix/telegramboot/config/BotConfig.java
@@ -0,0 +1,20 @@
+package com.sacavix.telegramboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import com.sacavix.telegramboot.service.TelegramBoot;
+
+@Configuration
+public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(TelegramBoot bot) throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+        return api;
+    }
+}
